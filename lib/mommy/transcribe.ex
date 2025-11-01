9cdd98288d5b1091f538ab4e59e7e6c950527087
P-rth <Parthsahni09@gmail.com>
2025-11-01 22:41:41 +0530

    Fix transcribe module to read the binary

--- lib/mommy/transcribe.ex
+++ lib/mommy/transcribe.ex
@@ -13,10 +13,22 @@ defmodule Mommy.Transcribe do
 			{:error, {:file_not_found, path}}
 		else
 			openai_client = OpenaiEx.new(Application.get_env(:openai_ex, :api_key))
-			audio_request = transcription_module.new(%{model: @default_model, file: path})
-			case transcription_module.create(openai_client, audio_request) do
-				{:ok, %{"text" => transcription_text}} -> {:ok, transcription_text}
-				{:error, reason} -> {:error, reason}
+
+			# Read the file as binary data
+			case File.read(path) do
+				{:ok, file_content} ->
+					# Create request with binary content and filename
+					audio_request = transcription_module.new(%{
+						model: @default_model,
+						file: {Path.basename(path), file_content}
+					})
+
+					case transcription_module.create(openai_client, audio_request) do
+						{:ok, %{"text" => transcription_text}} -> {:ok, transcription_text}
+						{:error, reason} -> {:error, reason}
+					end
+
+				{:error, reason} -> {:error, {:file_read_error, reason}}
 			end
 		end
 	end
